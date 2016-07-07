86f045dd5c2d2f8615c3faeaca8b59b6ffe1710d
g-ch <chg10086@yeah.net>
2016-07-07 19:53:25 +0800

    well predict

--- src/intersect_point.cpp
+++ src/intersect_point.cpp
@@ -20,9 +20,9 @@ float conterclockwise_rotate_angle = PI/2;
 struct robot_pose
 {
 	float x;
-	float x_history[4];
+	float x_history[10];
 	float y;
-	float y_history[4];
+	float y_history[10];
 	float theta;
 };
 
@@ -52,27 +52,42 @@ private:
 intersect_point::intersect_point()
 {
 	point_pub = n.advertise<geometry_msgs::Pose2D>("/intersect_point", 5);		//消息类型要改
-	robot_pose_sub = n.subscribe<geometry_msgs::Pose2D>("/position_filtered", 5, &intersect_point::robotCallBack, this);
+	robot_pose_sub = n.subscribe<geometry_msgs::Pose2D>("/robot_position", 5, &intersect_point::robotCallBack, this);
 	drone_pose_sub = n.subscribe<geometry_msgs::Pose2D>("/drone_pose", 5, &intersect_point::droneCallBack, this);		//消息类型要改
 }
 
 void intersect_point::robotCallBack(const geometry_msgs::Pose2D::ConstPtr& robot_pose)
 {
-	static float last_time;
-	float time_now = ros::Time::now().toSec();
-	if(time_now - last_time > 0.5){
+	static double last_time;
+	static float x_raw[20], y_raw[20];
+	static int cnt=0;
+	if(cnt<20){
+		x_raw[cnt] = robot_pose->x;
+		y_raw[cnt] = robot_pose->y;
+	}
+	cnt++;
+	double time_now = ros::Time::now().toSec();
+	if(time_now - last_time > 0.2){
+		float avrg_x, avrg_y;
+		for(int i = 0; i < cnt; i++){
+			avrg_x += x_raw[i];
+			avrg_y += y_raw[i];
+		}
+		avrg_x /= cnt;
+		avrg_y /= cnt;
+		cnt = 0;
 		last_time = time_now;
-		for(int i=3;i>0;i--){
+		for(int i=9;i>0;i--){
 			robot.x_history[i] = robot.x_history[i-1];
 		}
-		robot.x_history[0] = robot.x;
-		for(int i=3;i>0;i--){
+		robot.x_history[0] = avrg_x;
+		for(int i=9;i>0;i--){
 			robot.y_history[i] = robot.y_history[i-1];
 		}
+		robot.y_history[0] = avrg_y;
 	}
+//	ROS_INFO("Received");
 
-
-	robot.y_history[0] = robot.y;
 	robot.x = robot_pose->x;
 	robot.y = robot_pose->y;
 	robot.theta = robot_pose->theta;
@@ -87,12 +102,13 @@ void intersect_point::droneCallBack(const geometry_msgs::Pose2D::ConstPtr& drone
 
 void intersect_point::calc_intersect(robot_pose robot, const robot_pose drone)
 {
-	if (robot.theta < PI/2 || robot.theta > 3 * PI / 2)
-	{
-		intersect.x = drone.x;
-		intersect.y = drone.y;
-	}
-	else
+    static float last_y = 0;
+    //if (robot.theta < PI/2 || robot.theta > 3 * PI / 2)
+	//{
+		//intersect.x = drone.x;
+		//intersect.y = drone.y;
+	//}
+	//else
 	{
 		float dist = 1000;
 		float last_dist = 1000;
@@ -102,12 +118,13 @@ void intersect_point::calc_intersect(robot_pose robot, const robot_pose drone)
 
 		while(update)
 		{
-			//机器人运行的直线方程
+            update = false;
+            //机器人运行的直线方程
 		//	float k = tan(robot.theta);
 		//	float b = robot.y - robot.x * k;
-			Matrix<float, 4, 2> A;
-			Matrix<float, 4, 1> Y;
-			for(int i=0;i<4;i++){
+			Matrix<float, 10, 2> A;
+			Matrix<float, 10, 1> Y;
+			for(int i=0;i<10;i++){
 				A(i,0) = robot.x_history[i];
 				A(i,1) = 1;
 				Y(i,0) = robot.y_history[i];
@@ -118,6 +135,8 @@ void intersect_point::calc_intersect(robot_pose robot, const robot_pose drone)
 			MatrixXf KB = LU * Y;
 			float k = KB(0);
 			float b = KB(1);
+			ROS_INFO("\nx1: %f, x2: %f, x3: %f, x4: %f",robot.x_history[0],robot.x_history[1],robot.x_history[2],robot.x_history[3]);
+			ROS_INFO("\ny1: %f, y2: %f, y3: %f, y4: %f",robot.y_history[0],robot.y_history[1],robot.y_history[2],robot.y_history[3]);
 			ROS_INFO("\nk: %f\nb: %f", k, b);
 			
 			
@@ -125,11 +144,12 @@ void intersect_point::calc_intersect(robot_pose robot, const robot_pose drone)
 			//计算轨迹与场地边界的交点
 			float left_intersect_x = (WIDTH / 2 - b) / k;
 			float right_intersect_x = (-WIDTH / 2 - b) / k;
-			if (left_intersect_x < -HEIGHT || right_intersect_x < -HEIGHT)
-			{
+        //	if (left_intersect_x < -HEIGHT || right_intersect_x < -HEIGHT)
+            if(1)
+            {
 				if(robot.x_history[3]>robot.x_history[0]){
 					//如果交点在底线后面，直接计算飞机位置到机器人运行轨迹的垂线段交点
-					if(robot.x>-1.0){
+                    if(robot.x>-1.0){
 						intersect.x = -1.0;
 					}
 					else if(robot.x>-2.0){
@@ -140,57 +160,72 @@ void intersect_point::calc_intersect(robot_pose robot, const robot_pose drone)
 					}
 					else{
 						intersect.x = -3.3;
-					}
+                    }
+                    intersect.y = k * intersect.x + b;//if y close in 2 times let it stay still
+                    if(fabs(intersect.y) > WIDTH / 2){
+                        intersect.y = 0;
+                    }
+                    if(fabs(intersect.y - last_y) < 0.1){
+                        intersect.y = last_y;
+                    }
+                    else{
+                        last_y = intersect.y;
+                    }
+
 				}
+                else{
+                    intersect.x = -1000;
+                    intersect.y = -1000;
+                }
 			//	intersect.x = (drone.x / k + drone.y - b) / (k + 1/k);
-				intersect.y = k * intersect.x + b;
-				update = false;
+
+
 				break;
 			} 
-			else if (left_intersect_x > right_intersect_x)
-			{
-				//如果交点在场地侧壁，把机器人位置更新成交点
-				robot.theta += clockwise_rotate_angle;
-				robot.x = right_intersect_x;
-				robot.y = -WIDTH / 2;
-				//更新飞机到轨迹线段的距离
-				last_dist = dist;
-				dist = fabs(k * drone.x - drone.y + b) / sqrt(k * k + 1);
-				//如果距离比上一次大，则认为上一次是最近的拦截点，否则更新飞机到机器人运行轨迹的垂线段交点
-				if (dist > last_dist)
-				{
-					intersect.x = temp_x;
-					intersect.y = temp_y;
-					update = false;
-					break;
-				}
-				else
-				{
-					temp_x = (drone.x / k + drone.y - b) / (k + 1/k);
-					temp_y = k * intersect.x + b;
-				}
-			}
-			//场地另一侧边界的情况
-			else
-			{
-				robot.theta += conterclockwise_rotate_angle;
-				robot.x = left_intersect_x;
-				robot.y = WIDTH / 2;
-				last_dist = dist;
-				dist = fabs(k * drone.x - drone.y + b) / sqrt(k * k + 1);
-				if (dist > last_dist)
-				{
-					intersect.x = temp_x;
-					intersect.y = temp_y;
-					update = false;
-					break;
-				}
-				else
-				{
-					temp_x = (drone.x / k + drone.y - b) / (k + 1/k);
-					temp_y = k * intersect.x + b;
-				}
-			}
+//			else if (left_intersect_x > right_intersect_x)
+//			{
+//				//如果交点在场地侧壁，把机器人位置更新成交点
+//				robot.theta += clockwise_rotate_angle;
+//				robot.x = right_intersect_x;
+//				robot.y = -WIDTH / 2;
+//				//更新飞机到轨迹线段的距离
+//				last_dist = dist;
+//				dist = fabs(k * drone.x - drone.y + b) / sqrt(k * k + 1);
+//				//如果距离比上一次大，则认为上一次是最近的拦截点，否则更新飞机到机器人运行轨迹的垂线段交点
+//				if (dist > last_dist)
+//				{
+//					intersect.x = temp_x;
+//					intersect.y = temp_y;
+//					update = false;
+//					break;
+//				}
+//				else
+//				{
+//					temp_x = (drone.x / k + drone.y - b) / (k + 1/k);
+//					temp_y = k * intersect.x + b;
+//				}
+//			}
+//			//场地另一侧边界的情况
+//			else
+//			{
+//				robot.theta += conterclockwise_rotate_angle;
+//				robot.x = left_intersect_x;
+//				robot.y = WIDTH / 2;
+//				last_dist = dist;
+//				dist = fabs(k * drone.x - drone.y + b) / sqrt(k * k + 1);
+//				if (dist > last_dist)
+//				{
+//					intersect.x = temp_x;
+//					intersect.y = temp_y;
+//					update = false;
+//					break;
+//				}
+//				else
+//				{
+//					temp_x = (drone.x / k + drone.y - b) / (k + 1/k);
+//					temp_y = k * intersect.x + b;
+//				}
+//			}
 		}
 	}
 
@@ -208,7 +243,7 @@ void intersect_point::calc_intersect(robot_pose robot, const robot_pose drone)
 int main(int argc, char **argv)
 {
 	// float x_history[4] = {0,1,2,3};
-	// 		float y_history[4] = {1,2.2,2.8,4};
+	// 		float y_history[4] = {0,0,0,0};
 	// 		Matrix<float, 4, 2> A;
 	// 		Matrix<float, 4, 1> Y;
 	// 		for(int i=0;i<4;i++){
